468a62c13b90169ced34f425afe6d7242f9db8d9
Ankur Puri <apuri@iit.edu>
2018-05-15 22:31:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for(var i = 0; i < musicians.length; i++) {
-    
+    for(var j = 0; i < instruments.length; j++) {
+      var string = musicians[i] + " plays " + instruments[j]
+      array.push(string)
+    }  
   }
+  return array
 }
